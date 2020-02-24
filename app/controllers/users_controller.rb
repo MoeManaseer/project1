b0a15ffa3b239d0b6f16c735d702252207297088
Moutasem Manaseer <mo3manaseer@gmail.com>
2020-02-24 16:18:10 +0200

    Added some routes and session helpers.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def new
 
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      if user
+        flash.now[:error] = "Incorrect password."
+      else
+        flash.now[:error] = "You are not registerd."
+      end
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id]= nil;
+    redirect_to login_path
+  end
 end
